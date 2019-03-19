915d414618e9fc5ef3d369629eb88742580918ad
Anil Maharjan <anil@anilmaharjan.com.np>
2019-03-20 00:08:49 +0100

    SIM: Cleanup

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * SIM
+ * Paper & Pencil game on web
+ * author: anil@anilmaharjan.com.np
+ *
+ */
 class Sim {
   constructor(){
     this.movesLeft = [[1,2],[2,3], [3,4], [4,5], [5,6], [1,6], [1,3], [1,4], [1,5], [2,4], [2,5], [2,6], [3,5], [3,6], [4,6]];
@@ -6,11 +12,7 @@ class Sim {
     this.turn = 'playerA';
     this.winner = null;
   }
-  printMovesStatus(){
-    console.log("Moves left", this.movesLeft);
-    console.log("PlayerA", this.playerAMoves);
-    console.log("PlayerB", this.playerBMoves);
-  }
+
   move(move){
     const edge = this.movesLeft.splice(move,1,null)[0];
     if(edge){
@@ -28,6 +30,7 @@ class Sim {
       console.log("Move already used!");
     }
   }
+
   checkGameOver(edge){
     const edgesWithFirstVertex = this[this.turn+'Moves'].filter(d => d[0] === edge[0]);
     return edgesWithFirstVertex.some(d => {
