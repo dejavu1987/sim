cd6d475d2b37bce1fdeb78086f73efdf16d792a6
Anil Maharjan <anil@anilmaharjan.com.np>
2019-03-29 11:15:10 +0100

    SIM: change port to 80

--- src/index.js
+++ src/index.js
@@ -80,6 +80,6 @@ io.on('connection', function(socket){
 });
 
 // Start the server and listen to the port
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
